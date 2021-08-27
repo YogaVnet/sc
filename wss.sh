a12584312a90d28aeebe32e2defaf5979072f73c
YogaVnet <86974288+YogaVnet@users.noreply.github.com>
2021-08-27 16:42:28 +0700

    Update wss.sh

--- wss.sh
+++ wss.sh
@@ -2,16 +2,6 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl http://akses.endka.ga:81/aksesku | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
 clear
 figlet -f small Panel V2ray | lolcat
 echo -e ""
@@ -28,16 +18,16 @@ read -p "     Select From Options [1-4 or x] :  " port
 echo -e ""
 case $port in
 1)
-add-ws
+addws
 ;;
 2)
-del-ws
+delws
 ;;
 3)
-renew-ws
+renewws
 ;;
 4)
-cek-ws
+cekws
 ;;
 x)
 clear
